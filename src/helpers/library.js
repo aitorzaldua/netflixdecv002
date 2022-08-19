907e76b654b64232eba9825f2db2f437b60bf7b5
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-08-19 19:45:28 +0200

    add descriptions

--- src/helpers/library.js
+++ src/helpers/library.js
@@ -1,14 +1,4 @@
 
-
-import spideyLogo from "../images/spideyLogo.png";
-import spideyScene from "../images/spideyScene.png";
-import moralis from "../images/moralis.png";
-import moralisLogo from "../images/moralisLogo.png";
-import moralisScene from "../images/moralisScene.png";
-import jam from "../images/jam.png";
-import jamLogo from "../images/jamLogo.png";
-import jamScene from "../images/jamScene.png";
-
 import kaff from "../images/kaff.jpeg";
 import kingAff from "../images/the-kings-affection1.jpeg";
 import proposal from "../images/a-business-proposal.png";
@@ -22,7 +12,8 @@ import squid from "../images/Squid-Game.png";
 import vincenzo from "../images/vincenzo.png"
 
 import tka from "../images/tka.png";
-import abp from "../images/abp.png";
+import htcha from "../images/htcha.png";
+import squid2 from "../images/squid.png";
 
 export const movies = [
   {
@@ -43,43 +34,43 @@ export const movies = [
     Thumnbnail: proposal,
     Scene: proposal,
     Logo: "",
-    Name: "Spiderman Homecoming",
-    Description: "Peter Parker tries to stop the Vulture from selling weapons made with advanced Chitauri technology while trying to balance his life as an ordinary high school student.",
-    Genre: "Action, Superhero, Comedy",
-    Actors: "Tom Holland, Zendeya",
-    Year: "2017",
-    Duration: "2h 13min",
+    Name: "A bussines proposal",
+    Description: "Shin Ha-ri goes on a blind date after she accepts to replace her friend, Jin Young-seo, whose father had arranged it. The plan is to have Ha-ri 'getting rejected' by her prospective partner. However, it goes awry when her date turns out to be Kang Tae-moo, CEO of Go Food, the company where Ha-ri works.",
+    Genre: "Romance, Comedy",
+    Actors: "Kim Se-jeong, Ahn Hyo-seop, Seol In-ah",
+    Year: "2022",
+    Duration: "12 episodes",
   },
   {
     Movie: "https://gateway.pinata.cloud/ipfs/QmfVjDy9Z4KLhuDY5hbPkbHtC2eChnxHb173afk4xbbxFr",
     Thumnbnail: startup,
     Scene: startup,
-    Logo: spideyLogo,
-    Name: "Spiderman Homecoming",
-    Description: "Peter Parker tries to stop the Vulture from selling weapons made with advanced Chitauri technology while trying to balance his life as an ordinary high school student.",
-    Genre: "Action, Superhero, Comedy",
-    Actors: "Tom Holland, Zendeya",
-    Year: "2017",
-    Duration: "2h 13min",
+    Logo: "",
+    Name: "Start Up",
+    Description: "Seo Dal-mi is a bright and ambitious young woman who dreams of becoming Korea's Steve Jobs. Dal-mi doesn't have a fancy background but she's passionate about her work. She has bright energy and is a person of great vitality, having experience in a wide range of part-time jobs.",
+    Genre: "Action, Comedy",
+    Actors: "Bae Suzy, Nam Joo-hyuk",
+    Year: "2020",
+    Duration: "16 episodes",
   },
   {
     Movie: "https://gateway.pinata.cloud/ipfs/QmXoDy7o4Cd8G8vpYjzF49Ai7vozonfD1xF6Yyfdk1a7Pf",
     Thumnbnail: kingdom,
     Scene: kingdom,
-    Logo: moralisLogo,
-    Name: "Moralis Rap",
-    Description: "Moralis serverless Web3 Dapps, is a beautiful music video that tells a touching story of how anyone can become a Web3 developer if they put their trust in the hands of the musically gifted team at Moralis. ",
-    Genre: "Music Video",
-    Actors: "Ivan Liljeqvist, Filip Martinsson, CTO Larsson",
-    Year: "2021",
-    Duration: "1min 2sec",
+    Logo: "",
+    Name: "Kingdom",
+    Description: "Set during Korea's Joseon Dynasty, three years after the Imjin War, the first season of Kingdom follows the story of Crown Prince Lee Chang and his subordinates, who stumble across an unnatural plague that resurrects the dead amidst their investigation of a brewing political conspiracy and rumors of the King of Joseon's death.",
+    Genre: "Drama, Zombies",
+    Actors: "Ju Ji-hoon, Ryu Seung-ryong",
+    Year: "2019",
+    Duration: "12 episodes",
   },
   {
     Movie: "https://gateway.pinata.cloud/ipfs/QmX9xbzP2cixP1EmbxyNCFFR3fKo1Xar3UdfSTurWf2wn7",
     Thumnbnail: myName,
     Scene: myName,
-    Logo: jamLogo,
-    Name: "Jam Doughnut",
+    Logo: "",
+    Name: "My Name",
     Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
     Genre: "Silent Mukbang",
     Actors: "Jay",
@@ -90,7 +81,7 @@ export const movies = [
     Movie: "https://gateway.pinata.cloud/ipfs/QmeZrvMEegiho73jvveyhXrynARttHwuMQ3HRoCcLvKxZD",
     Thumnbnail: woo,
     Scene: woo,
-    Logo: jamLogo,
+    Logo: "",
     Name: "Jam Doughnut",
     Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
     Genre: "Silent Mukbang",
@@ -102,7 +93,7 @@ export const movies = [
     Movie: "https://gateway.pinata.cloud/ipfs/Qmd4i3V7WJc2KNtG2rQoCFQYwdxDMckaB3PEbXACYMToyw",
     Thumnbnail: chachacha,
     Scene: chachacha,
-    Logo: jamLogo,
+    Logo: htcha,
     Name: "Jam Doughnut",
     Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
     Genre: "Silent Mukbang",
@@ -114,7 +105,7 @@ export const movies = [
     Movie: "https://gateway.pinata.cloud/ipfs/QmYRdi6WDTowVnKdyjpG8oFSxpMT1JhBJf5MSrw7T6GawV",
     Thumnbnail: runon,
     Scene: runon,
-    Logo: jamLogo,
+    Logo: "",
     Name: "Jam Doughnut",
     Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
     Genre: "Silent Mukbang",
@@ -126,7 +117,7 @@ export const movies = [
     Movie: "https://gateway.pinata.cloud/ipfs/QmV3reeTockbC2bsfHKDW8Ljy7motYoLGJx6g7PQhx3Nac",
     Thumnbnail: squid,
     Scene: squid,
-    Logo: jamLogo,
+    Logo: squid2,
     Name: "Jam Doughnut",
     Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
     Genre: "Silent Mukbang",
@@ -138,7 +129,7 @@ export const movies = [
     Movie: "https://gateway.pinata.cloud/ipfs/QmSmQuJtCF6DVmHeuQrzgMRNaKV1Y49T3fiRFnfqupJWMt",
     Thumnbnail: vincenzo,
     Scene: vincenzo,
-    Logo: jamLogo,
+    Logo: "",
     Name: "Jam Doughnut",
     Description: "In this masterpiece a wild Jay, endulges in flavorful jam doughnut. Be warned, this video is most certainly going to leave you craving for some sweets.",
     Genre: "Silent Mukbang",
